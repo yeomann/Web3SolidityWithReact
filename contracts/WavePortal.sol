15e58365602838799c71d1d8ead6e83cb67f7f02
Danish Raza <danisraz@amazon.com>
2022-06-11 00:31:27 +0300

    * Added state variable and function for waving and counting waves

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+  uint256 totalWaves;
+
   constructor() {
     console.log("Yo yo, I am a contract and I am smart");
+  }
+
+  function AddWave() public {
+    totalWaves +=1;
+    console.log("%s has waved!", msg.sender);
+  }
+
+  function getTotalWavesCount() public view returns(uint256) {
+    console.log("Total waves count=%d", totalWaves);
+    return totalWaves;
   }
 }
