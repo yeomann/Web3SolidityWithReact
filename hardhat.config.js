7111db8102006e77363e036bad555a09520a9ab2
Danish Raza <danisraz@amazon.com>
2022-06-11 15:46:41 +0300

    * test deployment with goerli

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,15 @@
 require("@nomiclabs/hardhat-waffle");
+// Import and configure dotenv
+require("dotenv").config();
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = process.env.STAGING_GOERLI_ALCHEMY_KEY;
+const PROD_ALCHEMY_KEY = process.env.PROD_ALCHEMY_KEY;
+// Replace this private key with your Goerli account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +29,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [PRIVATE_KEY]
+    },
+    // mainnet: {
+    //   chainId: 1,
+    //   url: PROD_ALCHEMY_KEY,
+    //   accounts: [PRIVATE_KEY],
+    // },
+  }
 };
