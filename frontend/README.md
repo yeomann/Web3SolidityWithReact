7465212baa61329e379b43908789bed8ab8b3aee
Danish Raza <danisraz@amazon.com>
2022-06-11 15:47:57 +0300

    * added basic frontend react app

---
+++ frontend/README.md
@@ -0,0 +1,8 @@
+# Frontend for Wave Portal 👋 
+
+### **Welcome!**
+To get started with this course, clone this repo and follow these commands:
+
+1. Run `npm install` at the root of your directory
+2. Run `npm run start` to start the project
+3. Start coding!
---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+// import { ethers } from "ethers";
+import "./App.css";
+
+export default function App() {
+  const wave = () => {};
+
+  return (
+    <div className="mainContainer">
+      <div className="dataContainer">
+        <div className="header">
+          <span role="img" aria-label="wave">
+            👋
+          </span>{" "}
+          Hey there!
+        </div>
+
+        <div className="bio">
+          Hey, Connect your Ethereum wallet and wave at me!
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
+      </div>
+    </div>
+  );
+}
